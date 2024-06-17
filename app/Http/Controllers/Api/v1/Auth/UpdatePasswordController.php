8fef45286cad0e900b61f72ef3a7db203995892d
Jhonny Pirela <jhosagid7@gmail.com>
2024-06-17 14:55:44 -0400

    build(Api): :zap: Create update password request
    \

--- app/Http/Controllers/Api/v1/Auth/UpdatePasswordController.php
+++ app/Http/Controllers/Api/v1/Auth/UpdatePasswordController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Api\v1\Auth;
 
 use App\Classes\ApiResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\v1\User\UpdatePasswordRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UpdatePasswordController extends Controller
 {
-    public function Update(Request $request)
+    public function Update(UpdatePasswordRequest $request)
     {
         auth()->user()->update([
             'password' => Hash::make($request->password),
---
+++ app/Http/Requests/Api/v1/User/UpdatePasswordRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Api\v1\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'old_password'          => 'required|min:8',
+            'password'              => 'required|min:8|confirmed',
+        ];
+    }
+}
