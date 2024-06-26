e71351cbb346e00158226c06abbadc31eb3eeb80
Jhonny Pirela <jhosagid7@gmail.com>
2024-06-26 00:25:37 -0400

    chore(Api): refactori

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -48,7 +48,7 @@ class ResetPasswordNotification extends Notification
     public function toArray(object $notifiable): array
     {
         return [
-            //
+            //create the array representation of the notification
         ];
     }
 }
